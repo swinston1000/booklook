058f6263042cce562a798ce24bdb620fb04c8af5
Steven Winston <swinston100@hotmail.com>
2016-11-15 11:12:06 +0200

    changed to search by isbn

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 function BookLook() {
 
-    var displayBook = {}
-
     var displayBook = function(book) {
-        displayBook = book;
-        $("#display-book").empty();
-        var source = $('#book-template').html();
-        var template = Handlebars.compile(source);
-        var newHTML = template(displayBook);
-        $("#display-book").append(newHTML);
 
+        $("#display-book").empty();
+
+        // if book is an object we display it but if it is a number 
+        // then nothing was found so we need to display an error!
+        if (typeof book === "object") {
+            var source = $('#book-template').html();
+            var template = Handlebars.compile(source);
+            var newHTML = template(book);
+            $("#display-book").append(newHTML);
+
+        } else {
+            var source = $('#not-found-template').html();
+            var template = Handlebars.compile(source);
+            var newHTML = template({ isbn: book });
+            $("#display-book").append(newHTML);
+        }
     }
 
     var searchBook = function() {
@@ -27,17 +35,40 @@ var form = $('#book-form').parsley();
 
 $('#book-search').on('click', function(e) {
     if (form.validate()) {
-        var newBook = {};
-        newBook.title = $("#title-input").val();
-        newBook.author = $("#author-input").val();
-        newBook.description = $("#description-input").val();
-        newBook.image = $("#image-input").val();
-
-        newBook.pages = $("#pages-input").val();
-        newBook.minutes = $("#minutes-input").val();
-        app.displayBook(newBook);
+        var mins = $("#minutes-input").val();
+        var isbn = $("#isbn").val();
+        fetch(isbn, mins);
+        //empty form and remove validation css
+        $("#book-form").trigger('reset');
+        form.reset();
     }
 });
+
+var fetch = function(isbn, mins) {
+    $.ajax({
+        method: "GET",
+        url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:' + isbn,
+        dataType: "json",
+        success: function(data) {
+            if (data.totalItems > 0) {
+                var newBook = {};
+                newBook.title = data.items[0].volumeInfo.title;
+                newBook.author = data.items[0].volumeInfo.authors[0];
+                newBook.description = data.items[0].volumeInfo.description;
+                newBook.image = data.items[0].volumeInfo.imageLinks.thumbnail;
+                newBook.pages = data.items[0].volumeInfo.pageCount;
+                newBook.minutes = mins;
+                app.displayBook(newBook);
+            } else {
+                app.displayBook(isbn);
+            }
+
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.error(textStatus);
+        }
+    });
+};
 
 Handlebars.registerHelper("days", function(pages, minutes) {
     var days = pages / minutes;
